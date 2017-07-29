95d2e58e2e9998db05093e10aaf54f05a00e8788
Jay <jaysiu960216@gmail.com>
2017-07-29 16:00:25 -0700

    feature: allow acount admin to promote members to core and/or event admin(Chit Siu, Nathan Su)

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,6 +1,4 @@
-test = User.create!(:email => "datong@berkeley.edu", :password => "datong", :fname => "Admin", :lname => "Admin", :is_core_member => true, :is_event_admin => true, :is_account_admin => true)
-test.confirm
-test.save!
+User.create!(:email => "datong@berkeley.edu", :password => "datong", :fname => "Admin", :lname => "Datong", :is_core_member => true, :is_event_admin => true, :is_account_admin => true)
 #nathan = User.create!(:email => "nathan.su@berkeley.edu", :password => "password", :fname => "Nathan", :lname => "Su")
 #jay.skip_confirmation!
 #jay.save!
