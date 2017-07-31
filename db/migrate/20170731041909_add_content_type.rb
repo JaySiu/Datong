55d6aed13874e772b2bdb1e9aa566a7e1d10c736
Nathan <nathan.su@berkeley.edu>
2017-07-31 05:26:41 +0000

    debug: resources page file upload/download [Nathan Su, Jay Siu]

--- db/migrate/20170731041909_add_content_type.rb
+++ db/migrate/20170731041909_add_content_type.rb
@@ -1,5 +1,6 @@
 class AddContentType < ActiveRecord::Migration
   def change
     add_column :resources, :file_type, :string
+    add_column :resources, :file_contents, :binary
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -31,8 +31,8 @@ ActiveRecord::Schema.define(version: 20170731041909) do
   end
 
   create_table "resources", force: :cascade do |t|
-    t.string   "title",       null: false
-    t.string   "attachment",  null: false
+    t.string   "title",         null: false
+    t.string   "attachment",    null: false
     t.string   "description"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
