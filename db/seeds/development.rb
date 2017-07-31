74586c605a16f3379358310c16ba453ae9bf2655
Jay <jaysiu960216@gmail.com>
2017-07-30 21:36:25 -0700

    update heroku db

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,4 +1,6 @@
-User.create!(:email => "datong@berkeley.edu", :password => "datong", :fname => "Admin", :lname => "Datong", :is_core_member => true, :is_event_admin => true, :is_account_admin => true)
+admin = User.create!(:email => "datong@berkeley.edu", :password => "datong", :fname => "Admin", :lname => "Datong", :is_core_member => true, :is_event_admin => true, :is_account_admin => true)
+admin.confirm
+admin.save!
 #nathan = User.create!(:email => "nathan.su@berkeley.edu", :password => "password", :fname => "Nathan", :lname => "Su")
 #jay.skip_confirmation!
 #jay.save!
