e33e939d8ba7f7721b0d2ddbd2ecf2e466dba402
ZoeyHou <775165679@qq.com>
2017-08-06 06:09:00 +0000

    Change css and html

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,7 +21,11 @@
 
 $( document ).ready(function(){
     $(".button-collapse").sideNav();
-    
+});
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
     $(".imageRotation").each(function(){
         // parameters
         var imageRotation = this,
@@ -73,12 +77,6 @@ $( document ).ready(function(){
             setIntervalID=setInterval(rotate,intervalTime);
         });
     });
-    
-});
-
-$(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal-trigger').leanModal();
 });
 
 // Some jquery script to change a class when resizing to smaller than 992 or larger than 992
